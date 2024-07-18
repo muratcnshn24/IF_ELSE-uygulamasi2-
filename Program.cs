f12a85a7c9ea0f1c0f358d96852420a16739401f
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:20:34 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_ELSE_uygulamasi2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool result = true;
+
+            // başına ! işareti koyarsak else bloğu çalışacak.
+            if (!result) 
+            {
+                Console.WriteLine("Sonuc1");
+            }
+            else
+            {
+                Console.WriteLine("Sonuc2");
+            }
+
+        }
+    }
+}
